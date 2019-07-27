77caa11cc2e0069f57738c738119eebdd21ae806
eemrys <49338231+eemrys@users.noreply.github.com>
2019-07-27 23:29:34 +0300

    xml -> docx (back + front)
    тут "решение" в котором два проекта - консольное приложение (когда я просто писала бэк) и само приложение итоговое виндоус формс. поэтому в вижуал студио запускайте его

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Packaging;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public static void GenerateDocument(string xmlData, string template, string output)
+        {
+            if (File.Exists(output))
+            {
+                File.Delete(output);
+            }
+
+            File.Copy(template, output);
+
+            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(output, true))
+            {
+                MainDocumentPart mainPart = wordDoc.MainDocumentPart;
+                mainPart.DeleteParts(mainPart.CustomXmlParts);
+                CustomXmlPart myXmlPart = mainPart.AddCustomXmlPart(CustomXmlPartType.CustomXml);
+                using (FileStream stream = new FileStream(xmlData, FileMode.Open))
+                {
+                    myXmlPart.FeedData(stream);
+                }
+            }          
+        }
+    }
+}
---
+++ ConsoleApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConsoleApp1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string xmlData;
+        string template;
+        public Form1()
+        {
+            InitializeComponent();
+            Class2.GenerateXml();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "XML Files (*.xml)|*.xml";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.DefaultExt = "xml";
+            bool t = true;
+            while (t)
+            {
+                t = false;
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    if (!String.Equals(Path.GetExtension(openFileDialog1.FileName),
+                                       ".xml",
+                                       StringComparison.OrdinalIgnoreCase))
+                    {
+                        MessageBox.Show("The type of the selected file is not supported by this application. You must select an XML file.",
+                                        "Invalid File Type",
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Error);
+                        t = true;
+                    }
+                    else
+                    {
+                        xmlData = openFileDialog1.FileName;
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Word File (.docx)|*.docx";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.DefaultExt = "docx";
+            bool t = true;
+            while (t)
+            {
+                t = false;
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    if (!String.Equals(Path.GetExtension(openFileDialog1.FileName),
+                                       ".docx",
+                                       StringComparison.OrdinalIgnoreCase))
+                    {
+                        MessageBox.Show("The type of the selected file is not supported by this application. You must select a .docx file.",
+                                        "Invalid File Type",
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Error);
+                        t = true;
+                    }
+                    else
+                    {
+                        template = openFileDialog1.FileName;
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Class1.GenerateDocument(xmlData, template, "new_file.docx");
+        }
+    }
+}
